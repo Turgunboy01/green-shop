3896625b005da45d6c6e252a8bf64a7cd3ed4397
TurgunboyMakhmudjonov <turgunboy.channel@gmail.com>
2024-06-06 20:06:24 +0500

    upg

--- src/components/shop/ShopCard.jsx
+++ src/components/shop/ShopCard.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/CardSlice";
 import { SlBasket } from "react-icons/sl";
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const ShopCard = ({ item }) => {
+  const [like, setLike] = useState(false);
+
   const dispatch = useDispatch();
   const handleAddToCart = (product) => {
     let totalPrice = 1 * product.price;
@@ -19,7 +21,6 @@ const ShopCard = ({ item }) => {
   };
   return (
     <div>
-        
       <div className=" rounded  group m-4 ">
         <div className="bg-[#FBFBFB] relative overflow-hidden group-hover:border-t-[2px] border-t-[2px] border-t-[#4440] group-hover:border-t-[#46a359a1] ">
           <img
@@ -34,8 +35,15 @@ const ShopCard = ({ item }) => {
             >
               <SlBasket size={20} />
             </div>
-            <div className="hover:bg-[#fff] w-[100px] h-10 flex justify-center rounded-lg items-center hover:text-[#46A358]">
-              <FaRegHeart size={20} />
+            <div
+              onClick={() => setLike((prev) => !prev)}
+              className="hover:bg-[#fff] w-[100px] h-10 flex justify-center rounded-lg items-center hover:text-[#46A358]"
+            >
+              {!like ? (
+                <FaRegHeart size={20} />
+              ) : (
+                <FaHeart size={20} className="text-[#46A358]" />
+              )}
             </div>
             <Link
               to={`/shop/product/${item.id}`}
